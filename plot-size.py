c88c5e450be17add17221bc1eb1846697d943245
Harun Mbaabu Mwenda <mbaabuharun8@gmail.com>
2021-02-18 15:49:13 +0300

    Initial files

---
+++ plot-size.py
@@ -0,0 +1,19 @@
+# importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import random
+
+# plotting plot
+fig=plt.figure(figsize=(5,8))
+
+X=list(range(10))
+Y=[x+(x*random.random()) for x in X]
+
+plt.plot(X,Y)
+
+plt.title('Line Plot')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+
+plt.show()
